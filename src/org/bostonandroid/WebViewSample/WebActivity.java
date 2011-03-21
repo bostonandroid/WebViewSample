a7a3d1da3ce170d334292338ee763d457dfb8f76
Mike Burns <mburns@thoughtbot.com>
2011-03-21 17:58:40 -0400

    Make the sample app

---
+++ src/org/bostonandroid/WebViewSample/WebActivity.java
@@ -0,0 +1,15 @@
+package org.bostonandroid.WebViewSample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WebActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
