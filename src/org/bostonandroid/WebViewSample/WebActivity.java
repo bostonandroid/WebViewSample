58d2b4494e43c1c660bfbf4386d12da812420b5a
Mike Burns <mburns@thoughtbot.com>
2011-03-21 18:06:44 -0400

    Show the bostonandroid.org Web page

--- src/org/bostonandroid/WebViewSample/WebActivity.java
+++ src/org/bostonandroid/WebViewSample/WebActivity.java
@@ -3,13 +3,27 @@ package org.bostonandroid.WebViewSample;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class WebActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+
+public class WebActivity extends Activity {
+  private WebView page;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    page().loadUrl("http://bostonandroid.org/");
+  }
+
+
+  private WebView page() {
+    if (this.page == null) {
+      this.page = (WebView)findViewById(R.id.webpage);
+      WebSettings s = this.page.getSettings();
+      s.setJavaScriptEnabled(true);
     }
+    return this.page;
+  }
 }
